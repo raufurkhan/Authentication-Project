7a60c21531f729a60fa96ce0ade6cc990b30381e
raufurkhan <raufurkhan82@gmail.com>
2023-09-24 06:38:57 +0530

    Updating the Old Password

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -2,14 +2,18 @@ import { useState, useRef, useContext } from "react";
 import classes from "./AuthForm.module.css";
 // import { useHistory } from "react-router-dom";
 import AuthContext from "../../store/auth-context";
+import { useHistory } from "react-router-dom";
 
 const AuthForm = () => {
-  
+  console.log("a")
 
   const emailInputRef = useRef();
   const passwordInputref = useRef();
 
+  const history = useHistory();
   const authCtx = useContext(AuthContext);
+
+  
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -50,15 +54,18 @@ const AuthForm = () => {
         console.log(res);
         setIsLoading(false);
         if (res.ok) {
-          console.log("b")
           return res.json();
         } else {
-          console.log("k")
           return res.json().then((data) => {
 
             //show an error modal
 
             let errorMessage = "Aunthentication failed";
+
+                 // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+            // console.log(data);
 
             throw new Error(errorMessage);
 
@@ -67,6 +74,7 @@ const AuthForm = () => {
       })
       .then((data) => {
         authCtx.Login(data.idToken);
+        history.replace("/");
       })
       .catch((err) => {
         console.log(err.message)
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,51 @@
-import classes from './ProfileForm.module.css';
+
+
+import React, { useContext, useRef } from "react";
+import classes from "./ProfileForm.module.css";
+import AuthContext from "../../store/auth-context";
+import { useHistory } from "react-router-dom";
+
 
 const ProfileForm = () => {
+
+  const history = useHistory();
+
+  const newPasswordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAkBtS8XqPnFWiHAs2Qlb62O_oFyK1aj6Y",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "appplication/json",
+        },
+      }
+    ).then((res) => {
+      history.replace("/");
+    });
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input
+          type="password"
+          id="new-password"
+          minLength="7"
+          ref={newPasswordInputRef}
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
